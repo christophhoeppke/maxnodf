36f17b5287532d85cd840ae5f1101186b33ed601
Hoeppke <hoeppke@maths.ox.ac.uk>
2020-02-03 17:15:58 +0000

    updates to parallel code

--- src/fast_toolbox.cpp
+++ src/fast_toolbox.cpp
@@ -1,8 +1,7 @@
 #include <math.h>
 #include <iostream>
 #include <omp.h>
-#include <Rcpp.h>
-// #include <RcppArmadillo.h>
+#include <RcppArmadillo.h>
 // [[Rcpp::depends (RcppArmadillo)]]
 // [[Rcpp::plugins(openmp)]]
 // [[Rcpp::depends(RcppProgress)]]
